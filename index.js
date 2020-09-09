7b69ee4d8c0cfd86230cda970e057d681d4db528
rileysommerville <6712553+rileysommerville@users.noreply.github.com>
2020-09-09 20:09:25 +1000

    Add event listener for renderready

--- index.js
+++ index.js
@@ -8,3 +8,15 @@ r.style.width = "1px";
 r.style.height = "1px";
 r.src = "https://makecode.microbit.org/--docs?render=1"
 document.body.appendChild(r);
+
+// Events
+window.addEventListener("message", function (event) {
+    var msg = event.data;
+    if (msg.source != "makecode") return;
+
+    console.log(msg.type)
+    switch (msg.type) {
+        case "renderready":
+            // iframe is ready to receive render requests
+    }
+}, false);
