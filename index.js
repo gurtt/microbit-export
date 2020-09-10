37f7a44b43d8180140dce23ffb67bc30f3f3c27e
rileysommerville <6712553+rileysommerville@users.noreply.github.com>
2020-09-10 18:51:43 +1000

    Optimise CSS

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ r.style.left = 0;
 r.style.bottom = 0;
 r.style.width = "1px";
 r.style.height = "1px";
+r.style.display = "none";
 r.src = "https://makecode.microbit.org/--docs?render=1"
 document.body.appendChild(r);
 
